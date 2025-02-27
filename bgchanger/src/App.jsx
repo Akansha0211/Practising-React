061c75e80da0ef9817aabc132c533c8bd5a6cf8f
= <=>
2025-02-28 01:32:19 +0530

    password generator

---
+++ bgchanger/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  return (
+    <div className="bgScreen" style={{backgroundColor:color}}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">text</div>
+    </div>
+  )
+}
+
+export default App
