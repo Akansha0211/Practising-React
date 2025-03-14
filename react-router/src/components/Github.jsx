bfc2bb475d6b5b0611768cbcf5feeb40da3f3bd2
= <=>
2025-03-15 03:12:08 +0530

    calling api for getting github followers and image

--- react-router/src/components/Github.jsx
+++ react-router/src/components/Github.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Github() {
+  const [data,setData] = useState([]);
+  useEffect(()=>{
+    // component loads , useEffect is called
+    fetch('https://api.github.com/users/Akansha0211')
+    .then(response => response.json())
+    .then(data =>{
+      console.log(data);
+      setData(data);
+    })
+  }, [])
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
-      Github Followers:
+      Github Followers:{data.followers}
+      <img src={data.avatar_url} alt='Git picture' width={300}/>
     </div>
   )
 }
