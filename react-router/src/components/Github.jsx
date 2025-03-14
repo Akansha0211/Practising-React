01b30e4c48b35b60285dccc65ee78c499e299cea
= <=>
2025-03-15 03:30:09 +0530

    adding loader

--- react-router/src/components/Github.jsx
+++ react-router/src/components/Github.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-  const [data,setData] = useState([]);
-  useEffect(()=>{
-    // component loads , useEffect is called
-    fetch('https://api.github.com/users/Akansha0211')
-    .then(response => response.json())
-    .then(data =>{
-      console.log(data);
-      setData(data);
-    })
-  }, [])
+  // const [data,setData] = useState([]);
+  // useEffect(()=>{
+  //   // component loads , useEffect is called
+  //   fetch('https://api.github.com/users/Akansha0211')
+  //   .then(response => response.json())
+  //   .then(data =>{
+  //     console.log(data);
+  //     setData(data);
+  //   })
+  // }, [])
+
+  const data = useLoaderData()
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
       Github Followers:{data.followers}
@@ -20,3 +23,8 @@ function Github() {
 }
 
 export default Github
+
+export const githubInfoLoader = async () =>{
+  const response = await fetch('https://api.github.com/users/Akansha0211')
+  return response.json()
+}
