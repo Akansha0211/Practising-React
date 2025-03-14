bfc7e3d49183f10ee54025efdef85e6b1195aea4
= <=>
2025-03-15 02:41:13 +0530

    adding contact route

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './layout.jsx'
 import Home from './components/Home.jsx'
 import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
 
 const router = createBrowserRouter([
   {
@@ -19,6 +20,10 @@ const router = createBrowserRouter([
       {
         path: "about",
         element: <About/>
+      },
+      {
+        path:"contact",
+        element: <Contact/>
       }
     ]
   }
